19b504305ef4cc4e5798fb1a023ed0788acb97af
Siqxx <charlddoms@gmail.com>
2025-11-11 00:38:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js — starter connector for Admin page
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz9FmJzjWDQb4Klu3ba5rke_3Icv0qqHdi81mBDeAxbYLeX46GFD_9JQkuPJnu4vHLlyQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwMMzd_esncjg_bjGxB4rcDRMPswCclDa_jetOxgoBwoS4DnLIXY4zs_FyutIBIQSCmPA/exec';
 const DRIVE_UPLOAD_URL = 'https://script.google.com/macros/s/AKfycbx9efC0AU0DjFvHst8PPmRTffUVxc22UIMXbVVHMS2vS31KqnrUzIefasOijrARolxh4g/exec';
 const GOOGLE_CLIENT_ID = '507773507877-t1prpckunc9l2700dgfflhfk6jf2de5c.apps.googleusercontent.com';
 async function saveAdminRecord(form, photoFile, soiFile) {
